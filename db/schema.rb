848762230861b924e893a641d295d08889504e7e
jun-sugawara <ex.24karats.0110@i.softbank.jp>
2021-01-05 20:20:45 +0900

    最終調整

--- db/schema.rb
+++ db/schema.rb
@@ -34,22 +34,13 @@ ActiveRecord::Schema.define(version: 2021_01_05_094022) do
   end
 
   create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
     t.bigint "room_id"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["room_id"], name: "index_messages_on_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
-  end
-
-  create_table "messeges", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
-    t.bigint "room_id"
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["room_id"], name: "index_messeges_on_room_id"
-    t.index ["user_id"], name: "index_messeges_on_user_id"
   end
 
   create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -83,8 +74,6 @@ ActiveRecord::Schema.define(version: 2021_01_05_094022) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
-  add_foreign_key "messeges", "rooms"
-  add_foreign_key "messeges", "users"
   add_foreign_key "room_users", "rooms"
   add_foreign_key "room_users", "users"
 end
